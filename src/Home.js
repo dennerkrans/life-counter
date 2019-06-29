2e159f2e884a1b56a71ff79b3b0067b01e26df3c
Daniel Dennerkrans <dennerkrans@gmail.com>
2019-06-29 14:50:02 +0200

    stuff again

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,14 @@ function Home() {
   return (
     <div className="home">
       <Range
+        name="No. of players"
         options={[1, 2, 3, 4, 5, 6]}
         currentValue={playerCount}
         onSelect={e => setPlayerCount(Number(e.target.value))}
       />
       <Range
-        options={[20, 30, 40]}
+        name="Starting life"
+        options={[20, 30, 40, 60]}
         currentValue={startingLife}
         onSelect={e => setStartingLife(Number(e.target.value))}
         enableCustomPick={true}
--- src/components/Range.js
+++ src/components/Range.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
-function Range({ options, currentValue, onSelect, enableCustomPick = false }) {
+function Range({ name, options, currentValue, onSelect, enableCustomPick = false }) {
   const [inputActive, setInputActive] = useState(false)
   const inputRef = useRef(null)
   useEffect(() => {
@@ -14,29 +14,33 @@ function Range({ options, currentValue, onSelect, enableCustomPick = false }) {
 
   return (
     <div className="range">
-      {options.map(option => (
-        <button
-          className={`range-button ${option === currentValue ? 'range-button--current' : ''}`}
-          key={option}
-          value={option}
-          onClick={onSelect}
-        >
-          {option}
-        </button>
-      ))}
-      {enableCustomPick && (
-        <input
-          className={`range-input ${inputActive ? 'range-input--current' : ''}`}
-          ref={inputRef}
-          type="number"
-          onChange={onSelect}
-        />
-      )}
+      {name && <h2 className="range-name">{name}</h2>}
+      <div className="range-container">
+        {options.map(option => (
+          <button
+            className={`range-button ${option === currentValue ? 'range-button--current' : ''}`}
+            key={option}
+            value={option}
+            onClick={onSelect}
+          >
+            {option}
+          </button>
+        ))}
+        {enableCustomPick && (
+          <input
+            className={`range-input ${inputActive ? 'range-input--current' : ''}`}
+            ref={inputRef}
+            type="number"
+            onChange={onSelect}
+          />
+        )}
+      </div>
     </div>
   )
 }
 
 Range.propTypes = {
+  name: PropTypes.string,
   options: PropTypes.arrayOf(PropTypes.number).isRequired,
   currentValue: PropTypes.number,
   enableCustomPick: PropTypes.bool,
