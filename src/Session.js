2e9a17433154120db92c4171b41bffeaece189c7
Daniel Dennerkrans <dennerkrans@gmail.com>
2019-06-29 23:33:12 +0200

    Notes for next time

--- src/Session.js
+++ src/Session.js
@@ -1,5 +1,24 @@
 import React, { useState, useEffect } from 'react'
 
+/** Idea for session state structure example
+ * This should be saved in localStorage using the `sessionId` to identify
+ * the session. It should also update when the session state changes,
+ * i.e. Player 1 loses 2 life, new life total 18, or Player 1 changes name
+ * to `Daniel` update session state
+ * {
+ *   initialState: {
+ *     playerCount: 4,
+ *     startingLife: 20
+ *   }
+ *   players: [
+ *     { name: 'Player 1', life: 20 },
+ *     { name: 'Player 2', life: 13 },
+ *     { name: 'Player 3', life: 27 },
+ *     { name: 'Player 4', life: 11 },
+ *   ]
+ * }
+ */
+
 function Session({ sessionId }) {
   const [playerCount, setPlayerCount] = useState()
   const [startingLife, setStartingLife] = useState()
